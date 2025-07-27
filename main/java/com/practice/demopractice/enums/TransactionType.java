8975c7865d0be8d3b0dfc9ab2d3137492efd50c3
Anasiqbal596 <anasmughal596@gmail.com>
2025-07-27 17:20:06 +0500

    Added new Enums

--- main/java/com/practice/demopractice/enums/TransactionType.java
+++ main/java/com/practice/demopractice/enums/TransactionType.java
@@ -2,7 +2,7 @@ package com.practice.demopractice.enums;
 
 public enum TransactionType {
 
-        PEER,DOMESTIC,INTERNATIONAL,
-      REMITANCE,BILL,RAST,ONELINK,WALLET;
+
+      REMITANCE,PEER,DOMESTIC,INTERNATIONAL,DOMESTIC;
 
 }
